a444b8dbee1ace557470df6af47695b7daedb7c4
yahyameer <yahyeameer44@gmail.com>
2026-02-01 11:42:32 +0300

    feat: initialize project with Convex backend integration and Clerk authentication setup

---
+++ convex/auth.config.js
@@ -0,0 +1,9 @@
+
+export default {
+    providers: [
+        {
+            domain: process.env.CLERK_FRONTEND_API_URL,
+            applicationID: 'convex',
+        },
+    ],
+}
---
+++ src/app/ConvexClientProvider.tsx
@@ -0,0 +1,16 @@
+"use client";
+
+import { ConvexProvider, ConvexReactClient } from "convex/react";
+import { ConvexProviderWithClerk } from "convex/react-clerk";
+import { useAuth } from "@clerk/nextjs";
+import { ReactNode } from "react";
+
+const convex = new ConvexReactClient(process.env.NEXT_PUBLIC_CONVEX_URL!);
+
+export function ConvexClientProvider({ children }: { children: ReactNode }) {
+    return (
+        <ConvexProviderWithClerk client={convex} useAuth={useAuth}>
+            {children}
+        </ConvexProviderWithClerk>
+    );
+}
